68f007045cb5a1b463056a23b378c496506e6d45
Shuchasmitha <shuchasmithad@gmail.com>
2025-04-10 17:59:38 +0530

    ready to deploy

---
+++ backend/core/urls.py
@@ -0,0 +1,9 @@
+# core/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
+    path('api/surveys/', include('surveys.urls')),
+]
