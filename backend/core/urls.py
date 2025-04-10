5fb905eb13baeee9189cd696937e8312009846c5
Dasari Sai Yashwanth <116937254+Saiyashwanth7@users.noreply.github.com>
2025-04-10 21:19:17 +0530

    Update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,8 +1,11 @@
 # core/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
 def index(request):
     return JsonResponse({"message": "Survey API is working!"})
+    
 urlpatterns = [
     path('/', index),
     path('admin/', admin.site.urls),
